8b501999b8bea7585040fcace6dee8728aa8bc2e
Pedro Izecksohn <izecksohn@yahoo.com>
2022-09-27 16:04:07 -0300

    Add files via upload

--- csvgen.py
+++ csvgen.py
@@ -1,26 +1,14 @@
+#!/usr/bin/python3
 import datetime
 import csv
 
 personName = input ("Enter your full name: ")
 dt = datetime.datetime.now()
-#print (dt)
-allrows = []
-try:
-  f = open ("book.csv", "r", newline='')
-  cr = csv.reader (f)
-  for row in cr:
-      allrows.append(row)
-  f.close()
-except:
-  print ("I could not read book.csv .\nI'll create a new book.csv .")
 newrow = [str(dt), personName]
-allrows.append(newrow)
-#print (allrows)
 try:
-    f = open ("book.csv", "w", newline='')
+    f = open ("book.csv", "a", newline='')
     cw = csv.writer(f)
-    for row in allrows:
-        cw.writerow(row)
+    cw.writerow(newrow)
     f.close()
 except:
     print ("Error while saving.")
